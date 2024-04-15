1d57aceafc5a6a0ef5d30c613ddd23e323d07db0
Bradbury <1717930@mukilteo.wednet.edu>
2024-04-15 08:11:40 -0700

    saved us yall

--- Q4 Team 4 Game/Assets/Damon's Folder/Scripts/PlayerStats.cs
+++ Q4 Team 4 Game/Assets/Damon's Folder/Scripts/PlayerStats.cs
@@ -23,7 +23,7 @@ public class PlayerStats : MonoBehaviour
     [SerializeField] TextMeshProUGUI timer;
 
     
-    Damage player
+   
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if ((collision.gameObject.GetComponent("EnemyStats") as EnemyStats) != null)
