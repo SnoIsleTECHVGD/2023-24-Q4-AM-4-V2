e6701cd796571f70ad7a34b234dcb516343cf194
MUKILTEO\1600163 <1600163@mukilteo.wednet.edu>
2024-04-26 08:22:35 -0700

    crisis averted

---
+++ Q4 Team 4 Game/Assets/Joshua's Folder/MovementBackup.cs
@@ -0,0 +1,183 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBackup : MonoBehaviour
+{
+    public KeyCode left, right, jump;
+    public GameObject player, camera;
+    public float buildup = 1, jumpheight = 1, maxspeed = 1;
+
+    private Rigidbody2D rb2D;
+    private BoxCollider2D b2D;
+    private SpriteRenderer spriteRenderer;
+    public bool WASD = false;
+    public LayerMask groundLayer;
+    public float jumpbool;
+    private float boxCastFloat = 0.51f;
+    private bool grounded()
+    {
+        return Physics2D.BoxCast(transform.position - new Vector3(0f, boxCastFloat), new Vector2(jumpbool / 3, jumpbool), 0, Vector2.zero, 1, groundLayer);
+    }
+    private bool jumpHeld;
+    private float timeSinceJump = -5;
+    float cooldown = 0.2f;
+    private float xVelocity;
+    private float camAndPlayDist;
+    private float camPos;
+    private bool antiGravity = false;
+    private bool fhq = false;
+    private bool qhf = true;
+    private float aGravFloat = 1f;
+    private float aGravFloat2 = 1f;
+
+    float inputHorizontal;
+
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        b2D = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        rb2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), rb2D.velocity.y);
+
+        if (rb2D.velocity.x != 0f)
+            spriteRenderer.flipX = rb2D.velocity.x < 0f;
+        // Everything Between The Green Is God Mode
+
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            antiGravity = !antiGravity;
+        }
+        if (antiGravity)
+        {
+            rb2D.gravityScale = -rb2D.gravityScale;
+            antiGravity = !antiGravity;
+            fhq = !fhq;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            b2D.enabled = !b2D.enabled;
+        }
+        // Everything Between The Green Is God Mode
+        if (WASD == false)
+        {
+            if (Input.GetKey(left))
+            {
+                rb2D.AddForce(Vector2.left * buildup);
+                GetComponent<Animator>().SetInteger("State", 1);
+            }
+            else if (Input.GetKey(right))
+            {
+                rb2D.AddForce(Vector2.right * buildup);
+                GetComponent<Animator>().SetInteger("State", 1);
+            }
+            else
+            {
+                GetComponent<Animator>().SetInteger("State", 0);
+            }
+
+            if (WASD == true)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    rb2D.AddForce(Vector2.left * buildup);
+                    GetComponent<Animator>().SetInteger("State", 2);
+
+                }
+                else
+                {
+                    GetComponent<Animator>().SetInteger("State", 0);
+                }
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    rb2D.AddForce(Vector2.right * buildup);
+                    GetComponent<Animator>().SetInteger("State", 1);
+
+                }
+                else
+                {
+                    GetComponent<Animator>().SetInteger("State", 0);
+                }
+            }
+
+            if (grounded() && Input.GetKeyDown(jump) || (Time.time - cooldown < timeSinceJump && grounded()))
+            {
+
+                xVelocity = rb2D.velocity.x;
+                rb2D.velocity = new Vector3(xVelocity, jumpheight, 0);
+                cooldown = 0f;
+
+            }
+
+        }
+
+        // Control jump height with length of jump held
+        if (Input.GetKey(jump))
+        {
+            jumpHeld = true;
+            if (jumpHeld == true)
+            {
+
+                GetComponent<Animator>().SetInteger("State", 3);
+            }
+            else
+            {
+                GetComponent<Animator>().SetInteger("State", 0);
+            }
+        }
+        else { jumpHeld = false; }
+
+
+        if ((player.GetComponent<PlayerStats>().hasEatenAGFruit == false && !jumpHeld && !grounded() && rb2D.velocity.y > 0 && !fhq) || (player.GetComponent<PlayerStats>().hasEatenAGFruit == true && !jumpHeld && !grounded() && rb2D.velocity.y < 0 && fhq))
+        {
+            xVelocity = rb2D.velocity.x;
+            rb2D.velocity = new Vector3(xVelocity, 0, 0);
+        }
+
+        if (Input.GetKeyDown(jump) && !grounded())
+        {
+            timeSinceJump = Time.time;
+            cooldown = 0.2f;
+        }
+        rb2D.velocity = new Vector2(Mathf.Clamp(rb2D.velocity.x, -maxspeed, maxspeed), rb2D.velocity.y);
+        // Anti-Gravity Fruit functionality
+
+        if ((player.GetComponent<PlayerStats>().hasEatenAGFruit == true && qhf == true) || aGravFloat % 2 == 0)
+        {
+            camAndPlayDist = camera.transform.position.x - player.transform.position.x;
+            camera.transform.Rotate(0f, 0f, 180f);
+            player.transform.Rotate(0f, 0f, 180f);
+            camPos = camera.transform.position.x + (2 * camAndPlayDist);
+            camera.transform.position += new Vector3(2 * camAndPlayDist, 0, 0);
+            boxCastFloat = -boxCastFloat;
+            antiGravity = !antiGravity;
+            jumpheight = -jumpheight;
+            qhf = false;
+            aGravFloat2 += 1;
+        }
+        if (aGravFloat2 % 2 == 0 && player.GetComponent<PlayerStats>().fruitTimer == 0f)
+        {
+            qhf = true;
+            aGravFloat += 1;
+            aGravFloat2 += 1;
+        }
+        if (aGravFloat >= 3)
+        {
+            aGravFloat = 1;
+            aGravFloat2 = 1;
+        }
+    }
+    void WASDswap(bool WASDon)
+    {
+        WASD = WASDon;
+        WASDon = !WASDon;
+    }
+}
